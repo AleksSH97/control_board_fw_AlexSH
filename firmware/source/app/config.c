fda306d911a899a093b47aaabb28d324826bcff0
Alexandr Shabalin <shabalin@voltsbattery.com>
2023-06-12 21:39:07 +0300

    fw: drivers: wi-fi: add wifi ST reset delay fn
    descriptions: refactoring ST task with private functions

--- firmware/source/app/config.c
+++ firmware/source/app/config.c
@@ -58,12 +58,24 @@ ESS_CONFIG init_config = {
 
 CONFIG_DESCRIPTIONS config_description[] =
 {
-    {&config.beep_enabled,                    CONFIG_RECORD_TYPE_BOOL,     CONFIG_RECORD_GROUP_NONE,         0x00, "beep.enabled"       ,  {.bool_t   = {}}},
-    {&config.gprs.enabled,                    CONFIG_RECORD_TYPE_BOOL,     CONFIG_RECORD_GROUP_GPRS,         0x00, "gprs.enabled"       ,  {.bool_t   = {}}},
-    {&config.gprs.apn,                        CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_GPRS,         0x00, "gprs.apn"           ,  {.string_t = {31}}},
-    {&config.gprs.login,                      CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_GPRS,         0x00, "gprs.login"         ,  {.string_t = {15}}},
-    {&config.gprs.password,                   CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_GPRS,         0x00, "gprs.password"      ,  {.string_t = {15}}},
+    {&config.beep_enabled,                    CONFIG_RECORD_TYPE_BOOL,     CONFIG_RECORD_GROUP_NONE,         0x00, "beep.enabled"       ,  {.bool_t      = {}}},
+    {&config.gprs.enabled,                    CONFIG_RECORD_TYPE_BOOL,     CONFIG_RECORD_GROUP_GPRS,         0x00, "gprs.enabled"       ,  {.bool_t      = {}}},
+    {&config.gprs.apn,                        CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_GPRS,         0x00, "gprs.apn"           ,  {.string_t    = {31}}},
+    {&config.gprs.login,                      CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_GPRS,         0x00, "gprs.login"         ,  {.string_t    = {15}}},
+    {&config.gprs.password,                   CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_GPRS,         0x00, "gprs.password"      ,  {.string_t    = {15}}},
+    {&config.wifi.enabled,                    CONFIG_RECORD_TYPE_BOOL,     CONFIG_RECORD_GROUP_WIFI,         0x00, "wifi.enabled"       ,  {.bool_t      = {}}},
+    {&config.wifi.ssid,                       CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_WIFI,         0x00, "wifi.ssid"          ,  {.string_t    = {31}}},
+    {&config.wifi.passw,                      CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_WIFI,         0x00, "wifi.passw"         ,  {.string_t    = {31}}},
+    {&config.mqtt.pin,                        CONFIG_RECORD_TYPE_U32,      CONFIG_RECORD_GROUP_MQTT,         0x00, "mqtt.pin"           ,  {.uint32_t    = {10001, 99999}}},
+    {&config.mqtt.local,                      CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_MQTT,         0x00, "mqtt.local"         ,  {.string_t    = {31}}},
+    {&config.mqtt.host,                       CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_MQTT,         0x00, "mqtt.host"          ,  {.string_t    = {31}}},
+    {&config.mqtt.port,                       CONFIG_RECORD_TYPE_U16,      CONFIG_RECORD_GROUP_MQTT,         0x00, "mqtt.port"          ,  {.uint16_t    = {1, 65535}}},
+    {&config.mqtt.login,                      CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_MQTT,         0x00, "mqtt.login"         ,  {.string_t    = {15}}},
+    {&config.mqtt.passw,                      CONFIG_RECORD_TYPE_STRING,   CONFIG_RECORD_GROUP_MQTT,         0x00, "mqtt.passw"         ,  {.string_t    = {15}}},
+    {&config.mqtt.data_publish_timeout_s,     CONFIG_RECORD_TYPE_U8,       CONFIG_RECORD_GROUP_MQTT,         0x00, "mqtt.publish_s"     ,  {.uint8_t     = {5, 250}}},
 };
+
+CONFIG_INIT_RESULT init_result;
 
 /******************************************************************************/
 /* Private function prototypes ---------------------------------------------- */
