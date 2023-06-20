502d7f6769d32b1560d1fe0273e04c8b2182eaeb
Alexandr Shabalin <shabalin@voltsbattery.com>
2023-06-20 13:12:33 +0300

    fw: driver: ...: cmnds: cnsl_wi-fi: add new module
    description: this new folder for console different modules. This helps for readability of the code and for usability

---
+++ firmware/source/drivers/io/logging/commands/console_wi-fi.c
@@ -0,0 +1,135 @@
+/**
+ ******************************************************************************
+ * @file           : console.c
+ * @author         : Aleksandr Shabalin       <alexnv97@gmail.com>
+ * @brief          : Console system
+ ******************************************************************************
+ * ----------------- Copyright (c) 2023 Aleksandr Shabalin------------------- *
+ ******************************************************************************
+ ******************************************************************************
+ */
+
+/******************************************************************************/
+/* Includes ----------------------------------------------------------------- */
+/******************************************************************************/
+#include "console_wi-fi.h"
+
+#include "console.h"
+#include "esp/system/esp_ll.h"
+#include "esp/esp_sta.h"
+#include "esp/esp_private.h"
+
+
+/******************************************************************************/
+/* Private defines ---------------------------------------------------------- */
+/******************************************************************************/
+#define _CMD_HELP                   "help"
+#define _CMD_BACK                   "back"
+
+#define CONSOLE_MATCH               0u
+
+
+/******************************************************************************/
+/* Private function prototypes ---------------------------------------------- */
+/******************************************************************************/
+
+
+/******************************************************************************/
+
+
+
+
+/**
+ * @brief           WIFI menu for console
+ * @param[in]       @ref microrl_ptr pointer
+ * @param[in]       argc: shows how many arguments were entered on the command line
+ * @param[in]       argv: vector of string, it's elements are symbols
+ * @return          return 0 if everything is ok
+ */
+int ConsoleWiFi(microrl_t *microrl_ptr, int argc, const char * const *argv)
+{
+  int i = 0;
+  uint8_t res = 0x00;
+  ConsoleSetHelp(Console_WIFiPrintMenu);
+
+  while (i < argc)
+  {
+    if (strcmp(argv[i], "update") == CONSOLE_MATCH)
+    {
+        ConsoleClearScreen();
+        PrintfConsoleCRLF("\tUPDATING WIFI");
+        WiFiStop();
+        res = WiFiStart(WIFI_MODE_ST);
+
+        if (res != espOK)
+          PrintfConsoleCRLF("ERROR: START WI-FI");
+
+        esp_ll_deinit(NULL);
+        configure_uart(esp.ll.uart.baudrate);
+        //TODO PREPARE FOR ESP UPDATE IO UART
+        esp8266_update = true;
+
+    }
+    else if (strcmp(argv[i], "init") == CONSOLE_MATCH)
+    {
+      ConsoleClearScreen();
+      PrintfConsoleCRLF("\tINIT WIFI");
+      WiFiInit();
+    }
+    else if (strcmp(argv[i], _CMD_BACK) == CONSOLE_MATCH)
+    {
+      ConsoleBack();
+    }
+    else
+    {
+      ConsoleError();
+    }
+    i++;
+  }
+
+  return CONSOLE_OK;
+}
+/******************************************************************************/
+
+
+
+
+/**
+ * @brief          Print WI-FI menu
+ */
+void Console_WIFiPrintMenu(void)
+{
+  PrintfConsoleCRLF("");
+  PrintfConsoleCRLF("List of wi-fi commands:");
+  PrintfConsoleCRLF("\tupdate             -  update ESP");
+  PrintfConsoleCRLF("\tinit               -  init ESP");
+  PrintfConsoleCRLF("\tback               -  back to main menu");
+  PrintfConsoleCRLF("");
+}
+/******************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
