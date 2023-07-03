70c717aa83ef738f23a739a1738795b876af8f39
Alexandr Shabalin <shabalin@voltsbattery.com>
2023-07-03 19:58:47 +0300

    fw: lib: esp: system: esp_ll: add DMA for esp

--- firmware/lib/esp/system/esp_ll.c
+++ firmware/lib/esp/system/esp_ll.c
@@ -68,12 +68,19 @@
 #define ESP_CTRL_Pin LL_GPIO_PIN_15
 #define ESP_CTRL_GPIO_Port GPIOA
 
+#define ESP_USART_DMA_RX_BUFF_SIZE 0x1000
+#define ESP_MEM_SIZE 0x1000
+
+#if !defined(ESP_USART_RDR_NAME)
+#define ESP_USART_RDR_NAME RDR
+#endif /* !defined(LWESP_USART_RDR_NAME) */
+
 
 /******************************************************************************/
 /* Private variables -------------------------------------------------------- */
 /******************************************************************************/
-static osSemaphoreId_t esptxSemaphoreHandle;
-static osMutexId_t esptxMutexHandle;
+static osThreadId_t usart_ll_thread_id;
+static osMessageQueueId_t usart_ll_mbox_id;
 
 const osSemaphoreAttr_t esptxSemaphore_attr =
 {
@@ -92,10 +99,42 @@ const osMutexAttr_t esptxMutex_attr =
 };
 
 
-static uint8_t initialized;
-
+static uint8_t initialized, is_running;
+static uint8_t usart_mem[ESP_USART_DMA_RX_BUFF_SIZE];
+static size_t old_pos;
 
 /******************************************************************************/
+
+/**
+ * \brief           USART data processing
+ */
+static void
+usart_ll_thread(void* arg) {
+  size_t pos;
+
+  ESP_UNUSED(arg);
+
+  for (;;)
+  {
+    void* d;
+    /* Wait for the event message from DMA or USART */
+    osMessageQueueGet(usart_ll_mbox_id, &d, NULL, osWaitForever);
+
+    /* Read data */
+    pos = sizeof(usart_mem) - LL_DMA_GetDataLength(DMA1, LL_DMA_STREAM_7);
+    if (pos != old_pos && is_running) {
+      if (pos > old_pos) {
+        esp_input_process(&usart_mem[old_pos], pos - old_pos);
+      } else {
+        esp_input_process(&usart_mem[old_pos], sizeof(usart_mem) - old_pos);
+        if (pos > 0) {
+            esp_input_process(&usart_mem[0], pos);
+        }
+      }
+      old_pos = pos;
+    }
+  }
+}
 
 
 /**
@@ -103,95 +142,150 @@ static uint8_t initialized;
  */
 void
 configure_uart(uint32_t baudrate) {
-    if (!initialized)
-    {
-      LL_USART_InitTypeDef USART_InitStruct = {0};
-      LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-      LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_UART5);
-      __DSB();
+  static LL_USART_InitTypeDef USART_InitStruct;
+  static LL_DMA_InitTypeDef DMA_InitStruct;
+  LL_GPIO_InitTypeDef GPIO_InitStruct;
 
-      GPIO_InitStruct.Pin = ESP_RST_Pin;
-      GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
-      GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-      GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-      GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-      LL_GPIO_Init(ESP_RST_GPIO_Port, &GPIO_InitStruct);
-      LL_GPIO_SetOutputPin(ESP_RST_GPIO_Port, ESP_RST_Pin);
+  if (!initialized)
+  {
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_UART5);
+    __DSB();
 
-      GPIO_InitStruct.Pin = ESP_CTRL_Pin;
-      GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
-      GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-      GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-      GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-      LL_GPIO_Init(ESP_CTRL_GPIO_Port, &GPIO_InitStruct);
-      LL_GPIO_SetOutputPin(ESP_CTRL_GPIO_Port, ESP_CTRL_Pin);
+    GPIO_InitStruct.Pin = ESP_RST_Pin;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    LL_GPIO_Init(ESP_RST_GPIO_Port, &GPIO_InitStruct);
+    LL_GPIO_SetOutputPin(ESP_RST_GPIO_Port, ESP_RST_Pin);
 
-      if (esp8266_update)
-        LL_GPIO_ResetOutputPin(ESP_CTRL_GPIO_Port, ESP_CTRL_Pin);
+    GPIO_InitStruct.Pin = ESP_CTRL_Pin;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    LL_GPIO_Init(ESP_CTRL_GPIO_Port, &GPIO_InitStruct);
+    LL_GPIO_SetOutputPin(ESP_CTRL_GPIO_Port, ESP_CTRL_Pin);
 
-      GPIO_InitStruct.Pin = ESP_TX_Pin;
-      GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-      GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
-      GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-      GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
-      GPIO_InitStruct.Alternate = LL_GPIO_AF_8;
-      LL_GPIO_Init(ESP_TX_GPIO_Port, &GPIO_InitStruct);
+    if (esp8266_update)
+      LL_GPIO_ResetOutputPin(ESP_CTRL_GPIO_Port, ESP_CTRL_Pin);
 
-      GPIO_InitStruct.Pin = ESP_RX_Pin;
-      GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-      GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
-      GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-      GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
-      GPIO_InitStruct.Alternate = LL_GPIO_AF_8;
-      LL_GPIO_Init(ESP_RX_GPIO_Port, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = ESP_TX_Pin;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_8;
+    LL_GPIO_Init(ESP_TX_GPIO_Port, &GPIO_InitStruct);
 
-      NVIC_SetPriority(UART5_IRQn, 0x05);
-      NVIC_EnableIRQ(UART5_IRQn);
+    GPIO_InitStruct.Pin = ESP_RX_Pin;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_8;
+    LL_GPIO_Init(ESP_RX_GPIO_Port, &GPIO_InitStruct);
 
-      LL_USART_EnableIT_RXNE(UART5);
-      LL_USART_EnableIT_ERROR(UART5);
+    LL_USART_DeInit(UART5);
+    LL_USART_StructInit(&USART_InitStruct);
+    USART_InitStruct.BaudRate = baudrate;
+    USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
+    USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
+    USART_InitStruct.Parity = LL_USART_PARITY_NONE;
+    USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
+    USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
+    USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
+    LL_USART_Init(UART5, &USART_InitStruct);
 
-      USART_InitStruct.BaudRate = baudrate;
-      USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
-      USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
-      USART_InitStruct.Parity = LL_USART_PARITY_NONE;
-      USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
-      USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
-      USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
-      LL_USART_Init(UART5, &USART_InitStruct);
+    LL_USART_EnableIT_IDLE(UART5);
+    LL_USART_EnableIT_PE(UART5);
+    LL_USART_EnableIT_ERROR(UART5);
+    LL_USART_EnableDMAReq_RX(UART5);
 
-      LL_USART_DisableIT_CTS(UART5);
-      LL_USART_ConfigAsyncMode(UART5);
+    NVIC_SetPriority(UART5_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x05, 0x00));
+    NVIC_EnableIRQ(UART5_IRQn);
 
-      esptxSemaphoreHandle = osSemaphoreNew(1, 1, &esptxSemaphore_attr);
-      esptxMutexHandle = osMutexNew(&esptxMutex_attr);
+    is_running = 0;
 
-      LL_USART_Enable(UART5);
-    }
-    else
-    {
-        osDelay(10);
-        LL_RCC_ClocksTypeDef rcc_clocks;
-        LL_RCC_GetSystemClocksFreq(&rcc_clocks);
-        LL_USART_Disable(UART5);
-        LL_USART_SetBaudRate(UART5, rcc_clocks.PCLK1_Frequency, LL_USART_OVERSAMPLING_16, baudrate);
-        LL_USART_Enable(UART5);
-    }
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA1);
+    __DSB();
+
+    LL_DMA_DeInit(DMA1, LL_DMA_STREAM_7);
+    DMA_InitStruct.Channel = LL_DMA_CHANNEL_1;
+    DMA_InitStruct.PeriphOrM2MSrcAddress = (uint32_t)&UART5->DR;
+    DMA_InitStruct.MemoryOrM2MDstAddress = (uint32_t)usart_mem;
+    DMA_InitStruct.Direction = LL_DMA_DIRECTION_PERIPH_TO_MEMORY;
+    DMA_InitStruct.Mode = LL_DMA_MODE_CIRCULAR;
+    DMA_InitStruct.PeriphOrM2MSrcIncMode = LL_DMA_PERIPH_NOINCREMENT;
+    DMA_InitStruct.MemoryOrM2MDstIncMode = LL_DMA_MEMORY_INCREMENT;
+    DMA_InitStruct.PeriphOrM2MSrcDataSize = LL_DMA_PDATAALIGN_BYTE;
+    DMA_InitStruct.MemoryOrM2MDstDataSize = LL_DMA_MDATAALIGN_BYTE;
+    DMA_InitStruct.NbData = sizeof(usart_mem);
+    DMA_InitStruct.Priority = LL_DMA_PRIORITY_MEDIUM;
+
+//    LL_DMA_SetChannelSelection(DMA1, LL_DMA_STREAM_7, LL_DMA_CHANNEL_1);
+//    LL_DMA_ConfigTransfer(DMA1, LL_DMA_STREAM_7,
+//                          LL_DMA_DIRECTION_MEMORY_TO_PERIPH |
+//                          LL_DMA_PRIORITY_HIGH              |
+//                          LL_DMA_MODE_NORMAL                |
+//                          LL_DMA_PERIPH_NOINCREMENT         |
+//                          LL_DMA_MEMORY_INCREMENT           |
+//                          LL_DMA_PDATAALIGN_BYTE            |
+//                          LL_DMA_MDATAALIGN_BYTE);
+
+    LL_DMA_Init(DMA1, LL_DMA_STREAM_7, &DMA_InitStruct);
+
+    LL_DMA_EnableIT_HT(DMA1, LL_DMA_STREAM_7);
+    LL_DMA_EnableIT_TC(DMA1, LL_DMA_STREAM_7);
+    LL_DMA_EnableIT_TE(DMA1, LL_DMA_STREAM_7);
+    LL_DMA_EnableIT_FE(DMA1, LL_DMA_STREAM_7);
+    LL_DMA_EnableIT_DME(DMA1, LL_DMA_STREAM_7);
+
+    NVIC_SetPriority(DMA1_Stream7_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0x05, 0x00));
+    NVIC_EnableIRQ(DMA1_Stream7_IRQn);
+
+    old_pos = 0;
+    is_running = 1;
+
+    LL_DMA_EnableStream(DMA1, LL_DMA_STREAM_7);
+    LL_USART_Enable(UART5);
+
+
+//    esptxSemaphoreHandle = osSemaphoreNew(1, 1, &esptxSemaphore_attr);
+//    osSemaphoreAcquire(esptxSemaphoreHandle, 0);
+//    esptxMutexHandle = osMutexNew(&esptxMutex_attr);
+  }
+  else
+  {
+    osDelay(10);
+    LL_USART_Disable(UART5);
+    USART_InitStruct.BaudRate = baudrate;
+    LL_USART_Init(UART5, &USART_InitStruct);
+    LL_USART_Enable(UART5);
+  }
+
+  if (usart_ll_mbox_id == NULL) {
+    usart_ll_mbox_id = osMessageQueueNew(10, sizeof(void*), NULL);
+  }
+  if (usart_ll_thread_id == NULL) {
+    const osThreadAttr_t attr = {.stack_size = 1024};
+    usart_ll_thread_id = osThreadNew(usart_ll_thread, usart_ll_mbox_id, &attr);
+  }
 
     /*
      * Force ESP hardware reset
      * after initialization to make sure device is ready and
      * not in undefined state from previous AT usage
      */
-    if (!initialized) {
+  if (!initialized) {
 //KT2(ON);
-        LL_GPIO_ResetOutputPin(ESP_RST_GPIO_Port, ESP_RST_Pin);
-        osDelay(100);
-        LL_GPIO_SetOutputPin(ESP_RST_GPIO_Port, ESP_RST_Pin);
-        osDelay(200);
+    LL_GPIO_ResetOutputPin(ESP_RST_GPIO_Port, ESP_RST_Pin);
+    osDelay(100);
+    LL_GPIO_SetOutputPin(ESP_RST_GPIO_Port, ESP_RST_Pin);
+    osDelay(200);
 //KT2(OFF);
-    }
+  }
 }
 
 /**
@@ -200,46 +294,59 @@ configure_uart(uint32_t baudrate) {
  * \param[in]       len: Number of bytes to send
  * \return          Number of bytes sent
  */
-static size_t
-send_data(const void* data, size_t len) {
-//    if (!esp8266_update)
+static size_t send_data(const void* data, size_t len)
+{
+  if (!esp8266_update)
+  {
+    const uint8_t* d = data;
+
+//    PrintfLogsCont(CLR_MG"");
+//    for (uint8_t i = 0; i < len; i++)
 //    {
-//      size_t i;
-//      const uint8_t* d = data;
-//
-//      while (!LL_USART_IsActiveFlag_TXE(UART5));
-//      for (i = 0; i < len; i++, d++) {
-//          LL_USART_TransmitData8(UART5, *d);
-//          if (esp8266_debug)
-//            LL_USART_TransmitData8(UART4, *d);
-//          while (!LL_USART_IsActiveFlag_TXE(UART5));
-//      }
+//      PrintfLogsCont("%c", d[i]);
 //    }
-    if (esp8266_update) return (0);
-    volatile uint8_t *buf;
-    buf = (volatile uint8_t *) data;
+//    PrintfLogsCRLF(""CLR_DEF);
 
-    osMutexAcquire(esptxMutexHandle, osWaitForever);
-//    DMA_ConfigTxUART5(buf, len);
-//    LL_USART_EnableDMAReq_TX(UART5);
-//    LL_DMA_EnableIT_TC(DMA1, LL_DMA_STREAM_7);
-//    LL_DMA_EnableStream(DMA1, LL_DMA_STREAM_7);
-
-    PrintfLogsCont(CLR_MG"");
-    for (uint8_t i = 0; i < len; i++)
-      PrintfLogsCont("%c", buf[i]);
-    PrintfLogsCRLF(""CLR_DEF);
-//
-    while (!LL_USART_IsActiveFlag_TXE(UART5));
-    for (uint8_t i = 0; i < len; i++, buf++)
+    for (size_t i = 0; i < len; ++i, ++d)
     {
-      LL_USART_TransmitData8(UART5, *buf);
-      while (!LL_USART_IsActiveFlag_TXE(UART5));
+      LL_USART_TransmitData8(UART5, *d);
+      while (!LL_USART_IsActiveFlag_TXE(UART5)) {}
     }
-
-    //osSemaphoreAcquire(esptxSemaphoreHandle, osWaitForever);
-    osMutexRelease(esptxMutexHandle);
     return len;
+  }
+
+  if (esp8266_update)
+    return (0);
+
+//    osMutexAcquire(esptxMutexHandle, osWaitForever);
+//  volatile uint8_t *buf;
+//  buf = (volatile uint8_t *) data;
+//
+//  PrintfLogsCont(CLR_MG"");
+//  for (uint8_t i = 0; i < len; i++)
+//  {
+//    PrintfLogsCont("%c", buf[i]);
+//  }
+//  PrintfLogsCRLF(""CLR_DEF);
+//
+//  for (uint8_t i = 0; i < len; ++i, ++buf)
+//  {
+//    LL_USART_TransmitData8(UART5, *buf);
+//    while (!LL_USART_IsActiveFlag_TXE(UART5)) {}
+//  }
+//
+//  osSemaphoreAcquire(esptxSemaphoreHandle, osWaitForever);
+//  osMutexRelease(esptxMutexHandle);
+//  osMutexAcquire(esptxMutexHandle, osWaitForever);
+//  osMutexAcquire(dma174_MutexHandle, osWaitForever);
+//  DMA_ConfigTxUART5(buf, len);
+//  LL_USART_EnableDMAReq_TX(UART5);
+//  LL_DMA_EnableIT_TC(DMA1, LL_DMA_STREAM_7);
+//  LL_DMA_EnableStream(DMA1, LL_DMA_STREAM_7);
+//  osSemaphoreAcquire(esptxSemaphoreHandle, osWaitForever);
+//  osMutexRelease(dma174_MutexHandle);
+//  osMutexRelease(esptxMutexHandle);
+  return len;
 }
 
 /**
@@ -250,16 +357,19 @@ send_data(const void* data, size_t len) {
  * \return          Member of \ref espr_t enumeration
  */
 espr_t
-esp_ll_init(esp_ll_t* ll) {
+esp_ll_init(esp_ll_t* ll)
+{
     static uint8_t memory[0x6000];
     esp_mem_region_t mem_regions[] = {
-        { memory, sizeof(memory) }
+        {memory, sizeof(memory)}
     };
 
     if (!initialized) {
-        ll->send_fn = send_data;                /* Set callback function to send data */
+        esp_mem_assignmemory(mem_regions, ESP_ARRAYSIZE(mem_regions)); /* Assign memory for allocations */
+    }
 
-        esp_mem_assignmemory(mem_regions, ESP_ARRAYSIZE(mem_regions));  /* Assign memory for allocations */
+    if (!initialized) {
+        ll->send_fn = send_data;                /* Set callback function to send data */
     }
 
     configure_uart(ll->uart.baudrate);          /* Initialize UART for communication */
@@ -273,9 +383,21 @@ esp_ll_init(esp_ll_t* ll) {
  * \return          \ref espOK on success, member of \ref espr_t enumeration otherwise
  */
 espr_t
-esp_ll_deinit(esp_ll_t* ll) {
-    initialized = 0;                            /* Clear initialized flag */
-    return espOK;
+esp_ll_deinit(esp_ll_t* ll)
+{
+  if (usart_ll_mbox_id != NULL) {
+      osMessageQueueId_t tmp = usart_ll_mbox_id;
+      usart_ll_mbox_id = NULL;
+      osMessageQueueDelete(tmp);
+  }
+  if (usart_ll_thread_id != NULL) {
+      osThreadId_t tmp = usart_ll_thread_id;
+      usart_ll_thread_id = NULL;
+      osThreadTerminate(tmp);
+  }
+  initialized = 0;                    /* Clear initialized flag */
+  PROJ_UNUSED(ll);
+  return espOK;
 }
 
 #if !ESP_USE_TX_RX_INTERRUPT
@@ -315,11 +437,6 @@ void UART5_IRQHandler(void)
     PROJ_UNUSED(rx);
   }
 
-  if (LL_USART_IsActiveFlag_TXE(UART5))
-  {
-
-  }
-
   if (LL_USART_IsActiveFlag_RXNE(UART5))
   {
     if (esp8266_update)
@@ -343,64 +460,35 @@ void UART5_IRQHandler(void)
 #if ESP_USE_TX_RX_INTERRUPT
 void UART5_IRQHandler(void)
 {
-  uint8_t errors = 0;
+  LL_USART_ClearFlag_IDLE(UART5);
+  LL_USART_ClearFlag_PE(UART5);
+  LL_USART_ClearFlag_FE(UART5);
+  LL_USART_ClearFlag_ORE(UART5);
+  LL_USART_ClearFlag_NE(UART5);
+  IndicationLedYellowBlink(3);
 
-  if (LL_USART_IsActiveFlag_RXNE(UART5) && LL_USART_IsEnabledIT_RXNE(UART5))
+  if (usart_ll_mbox_id != NULL)
   {
-    if (esp8266_update)
-      LL_USART_TransmitData8(UART4, LL_USART_ReceiveData8(UART5));
-    else
-    {
-      uint8_t rx = LL_USART_ReceiveData8(UART5);
-
-#if !ESP_CFG_INPUT_USE_PROCESS
-      esp_input(&rx, 1);
-#endif
-
-#if ESP_CFG_INPUT_USE_PROCESS
-      esp_input_process(&rx, 1);
-#endif
-    }
+      void* d = (void*)1;
+      osMessageQueuePut(usart_ll_mbox_id, &d, 0, 0);
   }
-  else
+}
+
+
+/**
+ * \brief           UART DMA stream/channel handler
+ */
+void DMA1_Stream7_IRQHandler(void)
+{
+  LL_DMA_ClearFlag_TC7(DMA1);
+  LL_DMA_ClearFlag_HT7(DMA1);
+  IndicationLedGreenBlink(3);
+
+  if (usart_ll_mbox_id != NULL)
   {
-    if (LL_USART_IsActiveFlag_ORE(UART5))
-    {
-      //Read DR register for ORE flag reset
-      LL_USART_ClearFlag_ORE(UART5);
-      ++errors;
-    }
-    if (LL_USART_IsActiveFlag_FE(UART5))
-    {
-      //Read DR register for FE flag reset
-      LL_USART_ClearFlag_FE(UART5);
-      ++errors;
-    }
-    if (LL_USART_IsActiveFlag_NE(UART5))
-    {
-      //Read DR register for NE flag reset
-      LL_USART_ClearFlag_NE(UART5);
-      ++errors;
-    }
+      void* d = (void*)1;
+      osMessageQueuePut(usart_ll_mbox_id, &d, 0, 0);
   }
-
-//  if (LL_USART_IsActiveFlag_TC(UART5))
-//  {
-//    LL_USART_ClearFlag_TC(UART5);
-//    LL_USART_DisableIT_TC(UART5);
-//    osSemaphoreRelease(esptxSemaphoreHandle);
-//  }
-
-  if (errors != 0)
-  {
-    uint8_t rx = LL_USART_ReceiveData8(UART5);
-    PROJ_UNUSED(rx);
-  }
-
-//  if (LL_USART_IsActiveFlag_TXE(UART5))
-//  {
-//
-//  }
 }
 #endif /* !ESP_USE_TX_RX_INTERRUPT */
 
